e501b3b07058dcfcac20b390142f8f16848e8855
emergency-account <60676212+emergency-account@users.noreply.github.com>
2020-02-04 18:58:26 -0500

    first checkin
    Griselda real rap music. https//www.griseldaxfr.com

---
+++ Gene-SiminsExample/Gene-SiminsExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Gene_SiminsExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Gene-SiminsExample/Gene-SiminsExample/baseRapper.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gene_SiminsExample
+{
+    class baseRapper
+    {
+    }
+}
